170afdffbaa1d6f619873ba7d7cab3598d242224
far0010 <far0010@alu.ubu.es>
2025-02-26 21:06:32 +0100

    Paso5
    1ª parte

---
+++ P_tenis/script_tenis_procedures.sql
@@ -0,0 +1,42 @@
+/*
+https://github.com/far0010/abd2025/tree/main/P_tenis
+
+inicialmente cambiamos el aislamiento por defecto a SERIALIZABLE
+
+*/
+
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;
+
+drop table reservas;
+drop table pistas;
+drop sequence seq_pistas;
+
+create table pistas (
+	nro integer primary key
+	);
+	
+create table reservas (
+	pista integer references pistas(nro),
+	fecha date,
+	hora integer check (hora >= 0 and hora <= 23),
+	socio varchar(20),
+	primary key (pista, fecha, hora)
+	);
+
+create sequence seq_pistas;
+
+insert into pistas values (seq_pistas.nextval);
+insert into reservas 
+	values (seq_pistas.currval, '20/03/2018', 14, 'Pepito');
+insert into pistas values (seq_pistas.nextval);
+insert into reservas 
+	values (seq_pistas.currval, '24/03/2018', 18, 'Pepito');
+insert into reservas 
+	values (seq_pistas.currval, '21/03/2018', 14, 'Juan');
+insert into pistas values (seq_pistas.nextval);
+insert into reservas 
+	values (seq_pistas.currval, '22/03/2018', 13, 'Lola');
+insert into reservas 
+	values (seq_pistas.currval, '22/03/2018', 12, 'Pepito');
+
+commit;
